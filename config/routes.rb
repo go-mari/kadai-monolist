791c30074acbc9b01838f8e706509ebc2ebd52c3
go.mari <1karamari5@gmail.com>
2018-06-07 09:36:55 +0000

    ranking-have

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   get 'rankings/want', to: 'rankings#want'
   
+  get 'rankings/have', to: 'rankings#have'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
